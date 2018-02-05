56a0cdb8832b7270dbe43bb76ede16b51211532b
Oleksandr Diuriagin <sashadg@gmail.com>
2018-02-05 01:12:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var katzDeli = []
 var katzDeliLine = []
 
 function takeANumber (line,name) {
+  katzDeliLine.push(name)
   return "Welcome, "+ name + ". You are number +"line.length +" in line."
 }
